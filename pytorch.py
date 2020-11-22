16a72328591f3b064528cab6064924428951a7cc
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-22 18:39:11 +0000

    Add files via upload

--- pytorch.py
+++ pytorch.py
@@ -1,10 +1,26 @@
 import torch
 
+#vector
 t2 = torch.tensor([1.,2,3,4])
 
 print(t2)
 
 
+#matrix
+t3 = torch.tensor([[5., 6], [7, 8], [9, 10]])
+
+print(t3)
+
+#3d array
+
+t4 = torch.tensor([
+    [[11, 12, 13],
+     [13, 14, 15]]
+    [[15, 16, 17],
+     [17, 18, 19.]]])
+
+
+print(t4)
 
 
 
