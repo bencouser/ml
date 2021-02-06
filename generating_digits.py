6631bf9a4347855568ac041750eac3f3ad9c6675
bencouser <73387835+bencouser@users.noreply.github.com>
2021-02-06 14:51:42 +0000

    Add files via upload
    First attempt at generating digits using a DCGAN but having trouble with the formatting of the data.

---
+++ generating_digits.py
@@ -0,0 +1,99 @@
+print("Loading Libraries")
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+import os
+
+print("Preparing...")
+np.random.seed(42)
+tf.random.set_seed(42)
+
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "autoencoders"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+def plot_image(image):
+    plt.imshow(image, cmap="binary")
+    plt.axis("off")
+
+def plot_multiple_images(images, n_cols=None):
+    n_cols = n_cols or len(images)
+    n_rows = (len(images) - 1) // n_cols + 1
+    if images.shape[-1] == 1:
+        images = np.squeeze(images, axis=-1)
+    plt.figure(figsize=(n_cols, n_rows))
+    for index, image in enumerate(images):
+        plt.subplot(n_rows, n_cols, index + 1)
+        plt.imshow(image, cmap="binary")
+        plt.axis("off")
+
+print("Fetching Data")
+mnist = fetch_openml('mnist_784', version=1)
+X, y = mnist["data"], mnist["target"]
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+coding_size = 100
+
+print("Defining GAN")
+generator = keras.models.Sequential([
+    keras.layers.Dense(7 * 7 * 128, input_shape=[coding_size]),
+    keras.layers.Reshape([7, 7, 128]),
+    keras.layers.BatchNormalization(), 
+    keras.layers.Conv2DTranspose(64, kernel_size=5, strides=2, padding="SAME", activation="selu"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2DTranspose(1, kernel_size=5, strides=2, padding="SAME", activation="tanh"),
+    ])
+
+discriminator = keras.models.Sequential([
+    keras.layers.Conv2D(64, kernel_size=5, strides=2, padding="SAME", activation=keras.layers.LeakyReLU(0.2), input_shape=[28, 28, 1]),
+    keras.layers.Dropout(0.4),
+    keras.layers.Conv2D(128, kernel_size=5, strides=2, padding="SAME", activation=keras.layers.LeakyReLU(0.2)),
+    keras.layers.Dropout(0.4),
+    keras.layers.Flatten(),
+    keras.layers.Dense(1, activation="sigmoid")
+    ])
+
+def train_gan(gan, dataset, batch_size, coding_size, n_epochs):
+    generator, discriminator = gan.layers
+    for epoch in range(n_epochs):
+        print("Epoch {}/{}".format(epoch + 1, n_epochs))
+        for X_batch in dataset:
+            # Discriminator
+            noise = tf.random.normal(shape=[batch_size, coding_size])
+            generated_images = generator(noise)
+            X_fake_and_real = tf.concat([generated_images, X_batch], axis=0)
+            y1 = tf.constant([[0.]] * batch_size + [[1.]] * batch_size)
+            discriminator.trainable = True
+            discriminator.train_on_batch(X_fake_and_real, y1)
+            # Generator
+            noise = tf.random.normal(shape=[batch_size, coding_size])
+            y2 = tf.constant([[1.]] * batch_size)
+            discriminator.trainable = False
+            gan.train_on_batch(noise, y2)
+        plot_multiple_images(generated_images, 8)
+        plt.show()
+
+gan = keras.models.Sequential([generator, discriminator])
+
+discriminator.compile(loss="binary_crossentropy", optimizer="rmsprop")
+discriminator.trainable = False
+gan.compile(loss="binary_crossentropy", optimizer="rmsprop")
+
+X_train_dcgan = X_train.reshape(-1, 28, 28, 1) * 2. - 1.
+
+batch_size = 32
+dataset = tf.data.Dataset.from_tensor_slices(X_train_dcgan)
+dataset = dataset.shuffle(1000)
+dataset = dataset.batch(batch_size, drop_remainder=True).prefetch(1)
+
+train_gan(gan, dataset, batch_size, coding_size, n_epochs=1)
